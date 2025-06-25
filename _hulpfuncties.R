1f4974aa3e74670f3011a077bdba1a00121be04b
VERSCHELDE <pieter.verschelde@inbo.be>
2025-06-25 17:04:30 +0200

    improving the output document layout

---
+++ _hulpfuncties.R
@@ -0,0 +1,6 @@
+# Functie om kwantielen en gemiddelde te berekenen
+quantile_and_m <- function(x, probs = c(0.025, 0.5, 0.975), na.rm = TRUE, avg = TRUE) {
+  quantiles <- quantile(x, probs = probs, na.rm = na.rm)
+  mean_val <- mean(x, na.rm = na.rm)
+  return(if(!avg) quantiles else c(quantiles, avg = mean_val))
+}
